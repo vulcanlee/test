1caeac3f38b7e5ecc71a4ec47d94a4aa2043015e
Vulcan <vulcan.lee@gmail.com>
2020-03-20 19:37:53 +0800

    1

---
+++ HelloMVCCore/HelloMVCCore/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCCore.Controllers
+{
+    public class HelloModel
+    {
+        public string YourName { get; set; }
+        public string MyAnswer { get; set; }
+    }
+    public class HelloController : Controller
+    {
+        public HelloModel HelloModel { get; set; }
+        public HelloController()
+        {
+            HelloModel = new HelloModel();
+            HelloModel.YourName = "請在此輸入姓名";
+        }
+        public IActionResult Index()
+        {
+            return View(HelloModel);
+        }
+        [HttpPost]
+        public IActionResult Post(HelloModel model)
+        {
+            HelloModel.MyAnswer = $"{model.YourName} 你好，這裡是 ASP.NET Core Page";
+            return View(HelloModel);
+        }
+    }
+}
---
+++ HelloRazorPage/HelloRazorPage/Pages/Hello.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloRazorPage.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public string YourName { get;set; }
+        public string MyAnswer { get; set; }
+        public void OnGet()
+        {
+            YourName = "請在此輸入姓名";
+        }
+        public void OnPost(string yourName)
+        {
+            MyAnswer = $"{yourName} 你好，這裡是 Razor Page";
+        }
+    }
+}
