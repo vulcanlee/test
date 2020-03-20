2bd11fd67a3e4943e02d7c7ae8c29543e91f9b15
Vulcan <vulcan.lee@gmail.com>
2020-03-20 22:09:54 +0800

    Web Forms

---
+++ HelloWebForms/HelloWebForms/Hello.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Hello.aspx.cs" Inherits="HelloWebForms.Hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>你好 ASP.NET Web Forms</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        姓名: <asp:TextBox ID="YourName" runat="server"></asp:TextBox>
+        &nbsp;<br />
+        <asp:Button ID="btnPost" runat="server" Text="問安" />
+        <br />
+    </form>
+    <asp:Label ID="MyAnswer" runat="server" Text="Label"></asp:Label>
+</body>
+</html>
---
+++ HelloWebForms/HelloWebForms/Hello.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWebForms
+{
+    public partial class Hello : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                MyAnswer.Text = $"{YourName.Text} 你好，這裡是 ASP.NET Web Forms";
+                MyAnswer.Visible = true;
+            }
+            else
+            {
+                YourName.Text = "請在此輸入姓名";
+                MyAnswer.Visible = false;
+            }
+        }
+    }
+}
