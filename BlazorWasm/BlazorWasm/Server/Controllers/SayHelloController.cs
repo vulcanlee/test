56064127748d23843e7b48da15b217a077940426
Vulcan <vulcan.lee@gmail.com>
2020-03-21 15:08:54 +0800

    SayHell Blazor Client / Server

---
+++ BlazorWasm/BlazorWasm/Server/Controllers/SayHelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasm.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{yourName}")]
+        public string Get(string yourName)
+        {
+            return $"{yourName} 你好，這裡是 jQuery Ajax API";
+        }
+    }
+}
