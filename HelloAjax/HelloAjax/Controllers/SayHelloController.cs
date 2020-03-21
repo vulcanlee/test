22d94d85ad49548c23c7b5ff3a49974db698276b
Vulcan <vulcan.lee@gmail.com>
2020-03-21 14:02:13 +0800

    你好 jQuery Ajax

---
+++ HelloAjax/HelloAjax/Controllers/SayHelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAjax.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SayHelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{yourName}")]
+        public string Get(string yourName)
+        {
+            return $"{yourName} 你好，這裡是 jQuery Ajax API";
+        }
+
+    }
+}
