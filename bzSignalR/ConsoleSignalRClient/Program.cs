6ab9d1e56f4063cb901a1c51b792566b58841293
Vulcan <vulcan.lee@gmail.com>
2020-03-28 20:04:47 +0800

    SignalR

---
+++ bzSignalR/ConsoleSignalRClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleSignalRClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HubConnection _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/chatHub")
+                .Build();
+
+            _hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                var encodedMsg = $"{user}: {message}";
+                Console.WriteLine($"    {encodedMsg}");
+            });
+
+            await _hubConnection.StartAsync();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
