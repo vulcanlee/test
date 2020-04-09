2da337b583fa77acbf2bff2d133764344061f1a5
Vulcan <vulcan.lee@gmail.com>
2020-04-09 16:04:11 +0800

    .

---
+++ bzPropertyChanged/bzPropertyChanged/Data/Sample.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace bzPropertyChanged.Data
+{
+    public class Sample : INotifyPropertyChanged
+    {
+        private string firstName;
+
+        [Required(ErrorMessage = "欄位 名，不可為空白")]
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [Required(ErrorMessage = "欄位 姓，不可為空白")]
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [Range(0, 99, ErrorMessage = "年紀數值不正確")]
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
